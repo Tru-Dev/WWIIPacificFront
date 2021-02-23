7f61b096ed06f101c26f714a7123a67108977eea
Tru-Dev <29785998+Tru-Dev@users.noreply.github.com>
2021-02-23 12:18:02 -0700

    Initialized files, changed dependency

---
+++ game/main.py
@@ -0,0 +1,6 @@
+'''
+WWII Pacific Front - main.py
+(C) 2021 Jesus Trujillo, Delaney Siggia, Calvin Guela, Anthony Jaimes, Rocco Carrozza
+---
+This module is the entry point for the game client.
+'''
---
+++ game/multiplayer.py
@@ -0,0 +1,7 @@
+'''
+WWII Pacific Front - multiplayer.py
+(C) 2021 Jesus Trujillo, Delaney Siggia, Calvin Guela, Anthony Jaimes, Rocco Carrozza
+---
+This module is responsible for multiplayer capabilities.
+'''
+
