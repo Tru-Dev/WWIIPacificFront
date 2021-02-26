6312abb012dfb956a9943ffe0a29d36e271632f2
dts04 <79481847+dts04@users.noreply.github.com>
2021-02-26 13:40:08 -0700

    My commit.

--- game/main.py
+++ game/main.py
@@ -6,25 +6,23 @@ This module is the entry point for the game client.
 '''
 
 #this program should run a single-player battleship game against the computer
-#I comment 'elephant' where things need to be worked on. ctrl-f that if needed.
 
 import random
 from gamemodels import Ship
 
-#elephant (import other people's modules and work and everything)
-
+# TODO: import other people's modules and work and everything
 
 
 #this function allows the user to place their ships at a point of their choice
 def place_ship(board: Board, ship: Ship):
 
-    #the user should be able to place their ship horizontally or vertically
+    #the user should be able to place their ship horizont ally or vertically
     print("Press 1 to place your boat vertically or 2 to place your boat horizontally.")
     orientation = int(input)
 
     #places the boat vertically
     if orientation == 1:
-        print(f"The {ship.name} ship is {ship.length} tiles long.")
+        print(f"The {ship.name} ship is {ship.size} tiles long.")
         col = int(input("Select the column of the ship(0-15): "))
 
         #function continues if column is in bounds
@@ -32,12 +30,12 @@ def place_ship(board: Board, ship: Ship):
             row = int(input("Place the top-most point of the ship: "))
 
             #if column input is valid, adds ship coordinates
-            if row <= (15 - ship.length) and row > 0:
-                for i in range(ship.length):
+            if row <= (15 - ship.size) and row > 0:
+                for i in range(ship.size):
                     ship.coordinates.append(Tile(row+i, col))
 
             #error message if row input is invalid
-            elif row > (15-ship.length):
+            elif row > (15-ship.size):
                 print("This ship will not fit at this point. Try again.")
                 place_ship(board, ship)
 
@@ -58,7 +56,7 @@ def place_ship(board: Board, ship: Ship):
 
     #places the boat horizontally
     elif orientation == 2:
-        print(f"The {ship.name} ship is {ship.length} tiles long.")
+        print(f"The {ship.name} ship is {ship.size} tiles long.")
         row = int(input("Select the row of the ship(0-15): "))
 
         #function continues if row input is valid
@@ -66,12 +64,12 @@ def place_ship(board: Board, ship: Ship):
             col = int(input("Place the left-most point of the ship."))
 
             #if column input is valid, adds ship coordinates to list
-            if col <= (15 - ship.length) and col > 0:
-                for i in range(ship.length):
+            if col <= (15 - ship.size) and col > 0:
+                for i in range(ship.size):
                     ship.coordinates.append(Tile(row, col+i))
 
             #error message if column input is invalid
-            elif col > (15-ship.length):
+            elif col > (15-ship.size):
                 print("This ship will not fit at this point. Try again.")
                 place_ship(board, ship)
 
@@ -98,11 +96,12 @@ def place_ship(board: Board, ship: Ship):
         if tile.is_ship == True:
             print("A ship cannot be placed on top of another ship. Try again.")
             ship.coordinates = []
+            # TODO: this part needs to get rid of the previous coordinates and restart the function
             
 
 
 #this function allows the user to do everything required to take their turn
-def take_turn(Board(board)):
+def take_turn(board: Board):
     #the program should have the user select a tile to attack
     row = int(input("Select a row to attack(0-15): "))
     #the user should retry if the coordinates are out of bounds
@@ -115,24 +114,43 @@ def take_turn(Board(board)):
         if tile.attacked == False:
             tile.attacked = True
             #once an attack has been placed, it should be determined whether a ship has been hit
-            if tile.is_ship = True:
+            if tile.is_ship == True:
                 print("Hit")
                 #if a ship has been hit, it should determine whether it has sank and notify the user
-                #elephant (that ^)
+                # TODO: ^ find out which ship has been hit and check all of its coordinates to see if all the tiles have been hit
+                
+
             else:
                 print("Miss")
 
-    else:
-        print("This tile has already been hit. Try again")
-        take_turn
+        else:
+            print("This tile has already been hit. Try again")
+            take_turn
 
 #this function should have the program attack a spot on the user's board
 def comp_turn(board):
-    #elephant (lots needed here. make the computer take a turn.)
+    #program chooses a random spot on the grid to attack
+    row = random.randrange(16)
+    col = random.randrange(16)
+
+    # TODO: not entirely sure how to write this line. if the tile at [row],[col] has not been attacked:
+        print(f"The computer has attacked at row {row}, column {col}.")
+        tile.attacked = True
+
+        #determines whether or not a ship has been hit
+        if tile.is_ship == True:
+            print("One of your ships has been hit")
+        else:
+            print("None of your ships have been hit")
+
+    #if the tile has been hit already, the program will choose new numbers       
+    else:
+        comp_turn(board)
+
 
 #this function determines whether either player has won or lost
 def win_or_lose(board):
-    #elephant (all of this)
+    # TODO: all of this
     #this should check if all of one player's ships has sank
     #if there is no winner, the function should return False,
     #if there is a winner, the function should return True
@@ -160,10 +178,12 @@ for ship in ships:
 while 1:
     take_turn(comp_board)
     if win_or_lose(comp_board) == True:
-        print("You won")
+        print("All the opponents ships have been sunk. You win")
         break
 
     comp_turn(user_board)
     if win_or_lose(user_board) == True:
-        print("You lost")
+        print("All of your ships have been sunk. You lost")
         break
+
+# TODO: something should probably happen after the game ends. not sure what.
